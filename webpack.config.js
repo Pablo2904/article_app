77eff612bfddb546ad8c677dd4b8d8fb8fb7475f
Paweł Kocielny <pawel.koscielny2904@gmail.com>
2020-12-27 16:33:27 +0100

    package, and webpack improvments

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,6 @@ module.exports = {
       filename: "./index.html",
     }),
   ],
-  optimization: {
-    // minimize: true,
-  },
   module: {
     rules: [
       {
@@ -31,7 +28,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ["@babel/preset-react"],
           },
         },
       },
